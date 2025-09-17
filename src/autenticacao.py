f97433025e3ae95c32f8729f568c47ca5ccc2ed1
pridomtecnologia <suporte@pridom.com.br>
2025-09-16 23:33:39 -0300

    Lambda de autenticação

---
+++ src/autenticacao.py
@@ -0,0 +1,46 @@
+import json
+from src.rule.validar_cpf import validar_cpf
+from src.db.conexao import conexao, conn
+
+def autenticacao(event, context):
+    
+    try:
+        
+        body = json.loads(event.get("body") or "{}")
+        
+        cpf_cliente = body.get("cpf")
+
+        if not cpf_cliente:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"success": False, "error": "CPF é obrigatório"})
+            }
+
+        if not validar_cpf(cpf_cliente):
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"success": False, "error": "CPF inválido"})
+            }
+
+        consulta_cliente = conexao.execute("SELECT nome, cpf FROM cliente WHERE cpf = %s", (cpf_cliente,))
+        retorno_consulta_cliente = conexao.fetchone()
+        
+        conexao.close()
+        conn.close()
+
+        if retorno_consulta_cliente:
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"success": True, "cliente": consulta_cliente.nome, "cpf": consulta_cliente.cpf})
+            }
+        else:
+            return {
+                "statusCode": 404,
+                "body": json.dumps({"success": False, "error": "CPF não encontrado"})
+            }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"success": False, "error": str(e)})
+        }
--- src/handler.py
+++
@@ -1,4 +0,0 @@
-def handler(event, context):
-    params = (event.get("queryStringParameters") or {})
-    cpf = params.get("cpf")
-    return {"statusCode": 200, "body": f"OK - cpf={cpf}"}
